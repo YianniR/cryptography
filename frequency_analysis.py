fe510852c5314d62a6e9c83b0a47e154ae3fc358
YianniR <yiannis.rav@gmail.com>
2018-03-03 19:45:10 +0000

    Added vigenere solver

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -4,7 +4,8 @@ import codecs
 import os
 import matplotlib.pyplot as plt
 import itertools
-
+import re
+import numpy as np
 def read_hex(filename):
     with open(filename, 'rb') as f:
         hexdata = binascii.hexlify(f.read())
@@ -32,37 +33,70 @@ def split_cipher_text(cipher_text,key_length):
     for i in range(0,number_of_ciphers):
         new_cipher = []
         for j in range(0, len(cipher_text), key_length):
-            new_cipher.append(cipher_text[j+i])
+            try:
+                new_cipher.append(cipher_text[j+i])
+            except IndexError:
+                continue
         cipher_list.append(new_cipher)
-
     return cipher_list
 
 def merge_cipher_text(cipher_list,key_length):
     merged_cipher_text = []
-    for i in range(0,cipher_list):
-        for j in range(0, len(cipher_list)):
-            merged_cipher_text.append(cipher_list[j][i])
-        cipher_list.append(new_cipher)
+    for j in range(0, len(cipher_list[0])):
+        for i in range(0,len(cipher_list)):
+            try:
+                merged_cipher_text.append(cipher_list[i][j])
+            except IndexError:
+                continue
+    return merged_cipher_text
 
 def shift(cipher_text,shift):
     shifted_cipher_text = []
+
     for char in cipher_text:
-        char = ord(char)
-        if char+shift < 0:
-            new_char = 127 + char+shift
+        diff_from_a = (ord(char) + shift) - ord('a')
+        diff_from_z = (ord(char) + shift) - ord('z')
+        if diff_from_a < 0:
+            new_char = ord('z') + diff_from_a +1
+        elif diff_from_z > 0:
+            new_char = ord('a') + diff_from_z -1
         else:
-            new_char = char + shift
+            new_char = ord(char) + shift
+        #print(new_char)
         shifted_cipher_text.append(chr(new_char))
     return shifted_cipher_text
 
-hexdata = read_hex('secret.hex')
-cipher_text = bytes_to_char(hexdata)
-print(cipher_text)
-shifted_text = shift(cipher_text,-17)
-print(shifted_text)
+def conditioning(cipher_text):
+    regex = re.compile('[^a-zA-Z]')
+    cipher_text = regex.sub('', cipher_text).lower()
+    return cipher_text
 
-cipher_list = split_cipher_text(cipher_text,2)
+def find_shift(original,shifted):
+    shift = ord(original) - ord(shifted)
+    return shift
+
+cipher_text = "Zac xhjk hd zac glqklqsxlzl ugl rxtbomgugyrew zh rklr unrihkkl ml mcgvfoge og mxwcx mm mkyjx qznbkgry. Tlumfkk dagazbmt hd zac glqklqsxlzl gy mm ynnvhpz mfk ecgkloge vkmixqy, mfk eyzmcx isximyx fgl lum yrpyel zkxl kfnntqolcj. Xtowctvc yamcl rntr ymsjxlzl bkygtx y inpxbaaess uyyxb ug rnx yylcylkkgr ggb alc om ru zsowc zacok syx ml mgsx, yzmctmgug, ytw pklmakakl"
+cipher_text = conditioning(cipher_text)
+print(cipher_text)
+
+cipher_list = split_cipher_text(cipher_text,3)
+shifted_cipher_list = []
 for text in cipher_list:
     frequencies, alphabet = frequency_counter(text)
+
+    sorted_alphabet = [x for y, x in sorted(zip(frequencies, alphabet),reverse=True)]
     index = frequencies.index(max(frequencies))
-    most_common_char = alphabet[index]
+    print(sorted(frequencies))
+    if(cipher_list.index(text)==1):
+        most_common_char = sorted_alphabet[0]
+        shift_amount = find_shift('s',most_common_char)
+        shifted_cipher_text = shift(text,shift_amount)
+        shifted_cipher_list.append(shifted_cipher_text)
+    else:
+        most_common_char = sorted_alphabet[0]
+        shift_amount = find_shift('e',most_common_char)
+        shifted_cipher_text = shift(text,shift_amount)
+        shifted_cipher_list.append(shifted_cipher_text)
+
+decoded_cipher_text = merge_cipher_text(shifted_cipher_list,3)
+print(''.join(decoded_cipher_text))
